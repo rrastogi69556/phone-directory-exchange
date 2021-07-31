6843f858c49435a374614511cbbf1aa9c9ac9738
Roopanshu Rastogi <rrastogi69556@gmail.com>
2021-07-31 16:02:20 +0300

    Update ApiGatewayApplication.java
    Typo fix

--- api-gateway/src/main/java/com/impacttechs/assignment/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/impacttechs/assignment/apigateway/ApiGatewayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDistcoveryClien
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
